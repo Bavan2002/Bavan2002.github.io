4fec53ce9db695e91b4e05b21574e537c11b3fb3
Bavan2002 <91430828+Bavan2002@users.noreply.github.com>
2023-05-07 11:56:00 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,43 +1,17 @@
-// Define MQTT broker connection parameters
-var broker = {
-	hostname: "mqtt.eclipse.org",
-	port: 443,
-	path: "/mqtt",
-	username: "",
-	password: "",
-	useSSL: true
-};
-
-// Create a client instance
-var client = new Paho.MQTT.Client(broker.hostname, broker.port, broker.path);
-
-// Set callback handlers for MQTT client
-client.onConnectionLost = onConnectionLost;
-client.onMessageArrived = onMessageArrived;
-
-// Connect to MQTT broker
-client.connect({
-	onSuccess: onConnect,
-	userName: broker.username,
-	password: broker.password,
-	useSSL: broker.useSSL
+// Connect to the Cedalo MQTT broker
+var client = mqtt.connect("wss://2bit-iwud01ltywxm.cedalo.dev:443", {
+	username: "web",
+	password: "1234"
 });
 
-// Called when the client connects to MQTT broker
-function onConnect() {
-	console.log("Connected to MQTT broker");
-	client.subscribe("test/topic");
-}
+// Subscribe to the topic
+client.subscribe("TT");
 
-// Called when the client loses connection to MQTT broker
-function onConnectionLost(responseObject) {
-	if (responseObject.errorCode !== 0) {
-		console.log("Connection lost: " + responseObject.errorMessage);
-	}
-}
+// Handle incoming messages
+client.on("message", function (topic, message) {
+	// Update the topic label
+	$("#topic").text(topic);
 
-// Called when a message arrives on the subscribed topic
-function onMessageArrived(message) {
-	console.log("Message received: " + message.payloadString);
-	document.getElementById("message").innerHTML = message.payloadString;
-}
+	// Add the message to the list
+	$("#messages").prepend($("<li>").text(message.toString()));
+});
